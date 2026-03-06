dd45b87ae169b52016905176c262c4d24c63d0d0
Sadik Al Jarif <sadikaljarif05@gmail.com>
2026-03-06 21:39:13 +0600

    20th

--- 47.file.cpp
+++ 47.file.cpp
@@ -11,12 +11,41 @@ int main()
     // out << st;
     // out.close();
 
+    /*
+
     string st2;
     ifstream in("output.txt");
     // in >> st2; // it will only read the first word "hello"
 
     getline(in, st2);
     cout << st2;
+    */
+
+    // ofstream hout("output.txt");
+    // string name;
+    // cout << "Enter your name: ";
+    // cin >> name;
+    // hout << "Hello, " << name << "! Welcome to file handling in C++.";
+    // hout.close();
+
+    /*
+    ifstream hin("output.txt");
+    string greeting;
+    hin >> greeting;
+    cout << "this is the greeting: " << greeting;
+    hin.close();
+*/
+
+    ifstream in;
+    string st;
+    in.open("output.txt");
+
+    while (in.eof() == 0)
+    {
+        getline(in, st);
+        cout << st << endl;
+    }
+    in.close();
 
     return 0;
 }
